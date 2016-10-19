ee7fc360c9cdfd87c0db047b53ecc186341c6d97
misteramen <ndi15jle@student.hig.se>
2016-10-19 10:14:37 +0200

    Add files via upload

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+import gui.ApplicationFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationFrame app = new ApplicationFrame("Laboratioin 3");
+	}
+}
