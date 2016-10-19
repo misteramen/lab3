33434744860f9a9d1c44814d52d72184bb59627b
misteramen <ndi15jle@student.hig.se>
2016-10-19 10:17:09 +0200

    Delete PrimitivesPainter.java

--- src/gui/PrimitivesPainter.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import model.Vertex2D;
-
-public interface PrimitivesPainter {
-	public void paintPoint(Vertex2D v);
-	public void paintLine(Vertex2D v0, Vertex2D v1);
-	public void paintEllipse(Vertex2D v, double a, double b);
-}
