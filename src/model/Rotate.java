736905bd04a96df7686739c44ac2c522a065df3d
misteramen <ndi15jle@student.hig.se>
2016-10-27 11:26:10 +0200

    Update Rotate.java

--- src/model/Rotate.java
+++ src/model/Rotate.java
@@ -7,17 +7,16 @@ package model;
  * @author Andreas Brodin
  * @author Niklas Lindfors
  * @author Joshua Lutakome Yawe
- * @version 2.0
+ * @version 3.0
  *
  */
 public interface Rotate {
 	/**
 	 * This method applies rotation by a supplied <code>angle</code>.
 	 * 
-	 * @param xFactor - The factor by which the coordinates is to be scaled in the x-axis.
-	 * @param yFactor - The factor by which the coordinates is to be scaled in the y-axis.
+	 * @param angle - The angle by which the coordinates are to be rotated by.
 	 * 
-	 * @return Super - A new instance of the <code>Super</code> class is returned.
+	 * @return Figure - A new instance of the <code>Figure</code> class is returned.
 	 * 
 	 */
 	public Figure rotate(double angle);
