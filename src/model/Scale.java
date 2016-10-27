df141e1aa39986d97819848607a18a068baddef5
misteramen <ndi15jle@student.hig.se>
2016-10-27 11:26:40 +0200

    Update Scale.java

--- src/model/Scale.java
+++ src/model/Scale.java
@@ -7,17 +7,17 @@ package model;
  * @author Andreas Brodin
  * @author Niklas Lindfors
  * @author Joshua Lutakome Yawe
- * @version 2.0
+ * @version 3.0
  *
  */
 public interface Scale {
 	/**
 	 * This method applies scaling in the x and y-axis by the factors xFactor and yFactor.
 	 * 
-	 * @param xFactor - The factor by which the coordinates is to be scaled in the x-axis.
-	 * @param yFactor - The factor by which the coordinates is to be scaled in the y-axis.
+	 * @param xFactor - The factor by which the x-coordinate is to be scaled in the x-axis.
+	 * @param yFactor - The factor by which the y-coordinate is to be scaled in the y-axis.
 	 * 
-	 * @return Super - A new scaled instance of the <code>Super</code> class is returned.
+	 * @return Figure - A new scaled instance of the <code>Figure</code> class is returned.
 	 * 
 	 */
 	public Figure scale(double xFactor, double yFactor);
