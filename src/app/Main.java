f7875c74a2ec3824040b61f15ffe87c68b8379a1
misteramen <ndi15jle@student.hig.se>
2016-10-27 11:05:19 +0200

    Update Main.java
    Last Update

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,16 @@ package app;
 
 import gui.ApplicationFrame;
 
+/**
+ * This class' only task is to start the application by calling the 
+ * constructor to <code>ApplicationFrame</code>.
+ * 
+ * @author Andreas Brodin
+ * @author Niklas Lindfors
+ * @author Joshua Lutakome Yawe
+ * @version 1.0
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
 		new ApplicationFrame("Laboratioin");
