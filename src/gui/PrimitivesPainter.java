42e9121ba98e5a102e627a68170050a4dbeaae55
misteramen <ndi15jle@student.hig.se>
2016-10-27 11:18:35 +0200

    Update PrimitivesPainter.java

--- src/gui/PrimitivesPainter.java
+++ src/gui/PrimitivesPainter.java
@@ -2,6 +2,17 @@ package gui;
 
 import model.Vertex2D;
 
+/**
+ * <code>PrimitivesPainter</code> defines three paint*() methods which
+ * each aim to draw one of three basic geometric objects: A point, a Line 
+ * and an ellipse.
+ * 
+ * @author Andreas Brodin
+ * @author Niklas Lindfors
+ * @author Joshua Lutakome Yawe
+ * @version 1.0
+ *
+ */
 public interface PrimitivesPainter {
 	public void paintPoint(Vertex2D v);
 	public void paintLine(Vertex2D v0, Vertex2D v1);
