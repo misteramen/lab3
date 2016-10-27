b80009053b3440958961468ad76e41a38ca8dc5f
misteramen <ndi15jle@student.hig.se>
2016-10-27 11:17:19 +0200

    Update FigurePainter.java

--- src/gui/FigurePainter.java
+++ src/gui/FigurePainter.java
@@ -1,5 +1,15 @@
 package gui;
 
+/**
+ * The <code>FigurePainter</code> interface adds the functionality of painting
+ * a multitude of objects from one simple method.
+ * 
+ * @author Andreas Brodin
+ * @author Niklas Lindfors
+ * @author Joshua Lutakome Yawe
+ * @version 1.0
+ *
+ */
 public interface FigurePainter {
 	public void paintAll();
 }
