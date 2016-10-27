8be0403b260c3075c79cd67eb346112d5f47a1cc
misteramen <ndi15jle@student.hig.se>
2016-10-27 11:15:34 +0200

    Update Drawable.java

--- src/gui/Drawable.java
+++ src/gui/Drawable.java
@@ -1,5 +1,15 @@
 package gui;
 
+/**
+ * The <code>Drawable</code> interface enables a class to be drawn by an implemented
+ * instance of the PrimitivePainter interface.
+ * 
+ * @author Andreas Brodin
+ * @author Niklas Lindfors
+ * @author Joshua Lutakome Yawe
+ * @version 1.0
+ *
+ */
 public interface Drawable {
 	public void draw(PrimitivesPainter painter);
 }
